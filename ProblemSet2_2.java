aac46297d8efffe0531dc3b96b35686414f717a7
rinzels <113381365+rinzels@users.noreply.github.com>
2022-11-16 13:49:11 -0500

    problemset2 #1

--- ProblemSet2_2.java
+++ ProblemSet2_2.java
@@ -2,6 +2,15 @@ public class ProblemSet2_2 {
     public static void main(String[] args) {
         //Use print statements to create your own beautiful original ASCII art.
         //Use comments to describe what your image is depicting.
+    	System.out.println("    /\\    /\\  ");
+    	System.out.println("   /  \\__/  \\ ");
+    	System.out.println("  /          \\");
+    	System.out.println(" (  /\\    /\\  )");
+    	System.out.println("===     V   ====");
+    	System.out.println("====(___I___)===");
+    	System.out.println("  (           ) ");
+    	System.out.println("   (_________)  ");
+    	
         
         
         
